624ebb8db0e49b1c1c5943a7d69dc8e5583dda12
Eduk <ekakhrisnanda@gmail.com>
2025-02-06 14:34:13 +0800

    feat: create a displayBook function
    - Create a card container on html
    - Get the element and store it in cardWrapper
    - Loop through the array and display properties inside cardWrapper
    - Style the card

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-const myLibrary = [];
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read) 
-    myLibrary.push(book);
-}
+const cardWrapper = document.querySelector('.card-wrapper');
 
 const myLibrary = [];
 
@@ -26,5 +14,29 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-addBookToLibrary('The Dip', 'Seth Godin', 93, 'Have read');
-addBookToLibrary('Range', 'David Epstein', 390, 'Not yet');
+addBookToLibrary('The Dip', 'Seth Godin', 93, 'No');
+addBookToLibrary('Range', 'David Epstein', 390, 'Yes');
+addBookToLibrary('The Dip', 'Seth Godin', 93, 'No');
+addBookToLibrary('Range', 'David Epstein', 390, 'Yes');
+addBookToLibrary('The Dip', 'Seth Godin', 93, 'No');
+addBookToLibrary('Range', 'David Epstein', 390, 'Yes');
+addBookToLibrary('The Dip', 'Seth Godin', 93, 'No');
+addBookToLibrary('Range', 'David Epstein', 390, 'Yes');
+
+
+function displayBook() {
+    myLibrary.forEach((book) => {
+        const card = `
+            <div class="card">
+                <h2>${book.title}</h2>
+                <h3>${book.author}</h3>
+                <h4>${book.pages} pages</h4>
+                <p>Read Status: ${book.read}</p>
+            </div>
+        `;
+
+        cardWrapper.innerHTML += card;
+    }); 
+}
+
+displayBook();
